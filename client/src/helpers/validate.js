2f86aa7b8f7cf3d08fd7c1fc0220bbc33f2ffcfc
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-08 23:35:49 +0600

    password validation

--- client/src/helpers/validate.js
+++ client/src/helpers/validate.js
@@ -7,6 +7,45 @@ export const usernameValidate = async (values) => {
   return errors;
 };
 
+/** validate password */
+export const passwordValidate = async (values) => {
+  const errors = passwordVerify({}, values);
+
+  return errors;
+};
+
+/** validate reset password */
+export const resetPasswordValidation = async (values) => {
+  const errors = passwordVerify({}, values);
+
+  if (values.password !== values.confirm_pwd) {
+    errors.exist = toast.error('Password not match...!');
+  }
+
+  return errors;
+};
+
+/** ************************************************* */
+
+/** validate password */
+const passwordVerify = (errors = {}, values) => {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (!values.password) {
+    errors.password = toast.error('Password Required...!');
+  } else if (values.password.includes(' ')) {
+    errors.password = toast.error('Wrong Password...!');
+  } else if (values.password.length < 6) {
+    errors.password = toast.error(
+      'Password must be more than 6 characters long'
+    );
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error('Password must have special character');
+  }
+
+  return errors;
+};
+
 /** validate username */
 const usernameVerify = (error = {}, values) => {
   if (!values.username) {
