30cfc2298e7018aca2e1479b66b7b055b7bbbc20
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-08 23:05:08 +0600

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Register from './components/Auth/Register';
+import Password from './components/Profile/Password';
+import Profile from './components/Profile/Profile';
+import Recovery from './components/Profile/Recovery';
+import Reset from './components/Profile/Reset';
+import Username from './components/Profile/Username';
+import NotFound from './components/Shared/NotFound';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Username />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password" element={<Password />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/recovery" element={<Recovery />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
