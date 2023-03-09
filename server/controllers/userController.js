4485231dac042f15a581ab23f84bd8f1fce51b84
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-09 18:32:26 +0600

    user controllers functions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,23 @@
 /** GET: /api/user/example123 */
 const getUser = async (req, res) => {
-  res.json('getUser route');
+  const { username } = await req.params;
+  try {
+    const data = await User.find({ username }).select({
+      __v: 0,
+      createdAt: 0,
+      updatedAt: 0,
+    });
+    res.status(200).json({
+      message: 'SUCCESS!!',
+      data,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'There is a server side error!',
+      // error: err
+    });
+  }
 };
 
 /** PUT: /api/user/updateuser 
@@ -14,7 +31,38 @@ body: {
 }
 */
 const updateUser = async (req, res) => {
-  res.json('updateUser route');
+  const { userId } = req.user;
+  const updatedUserInfo = req.body;
+  const opts = { runValidators: true };
+  if (mongoose.Types.ObjectId.isValid(userId)) {
+    User.findByIdAndUpdate(
+      { _id: userId },
+      {
+        $set: updatedUserInfo,
+      },
+      {
+        opts,
+      },
+      (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            message: 'There is a server side error!',
+            // error: err
+          });
+        } else {
+          res.status(200).json({
+            message: 'User updated successfully!!',
+            data: updatedUserInfo,
+          });
+        }
+      }
+    ).clone();
+  } else {
+    res.status(500).json({
+      message: 'There is a server side error!',
+    });
+  }
 };
 
 module.exports = {
